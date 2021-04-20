410a6d1e4c7496e7f5104bb48a23e703da4687c7
Luca Peppe <lucapeppe31@gmail.com>
2021-04-20 22:07:50 +0200

    Add get_service_status function.

--- lib/phx_healthcheck/healthcheck/healthcheck.ex
+++ lib/phx_healthcheck/healthcheck/healthcheck.ex
@@ -1,3 +1,8 @@
 defmodule PhxHealthcheck.Healthcheck do
   @callback check_status() :: String.t()
+
+  @spec get_service_status(atom()) :: String.t()
+  def get_service_status(service) do
+    GenServer.call(service, {:get_status})
+  end
 end
